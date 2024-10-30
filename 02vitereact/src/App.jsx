02a561f61deb26990f00f8ae50eec8371d95c8d2
Sourabhkm7 <sourabhhithaldia@gmail.com>
2024-10-30 23:35:49 +0530

    02Vite+react

---
+++ 02vitereact/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ let counter = 15
+
+ const addValue = ()=>{
+  console.log("value added", Math.random())
+  counter = counter+1
+  console.log(counter)
+ }
+   return (
+    <>
+      <h1> Counter Using hooks</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value</button>
+      <br/>
+      <button>Decrease value</button>
+    </>
+  )
+}
+
+export default App
