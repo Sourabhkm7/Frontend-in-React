2c22676ac787d1fe555ff65120b7269299b05fba
Sourabhkm7 <sourabhhithaldia@gmail.com>
2024-11-07 00:44:20 +0530

    UI Creation

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -1,10 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/contact'
+import User from './components/User/User'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children:[
+      {
+        path: "",
+        element: <Home />
+
+      },{
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      },
+      {
+        path:'user/:userid',
+        element: <User/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
