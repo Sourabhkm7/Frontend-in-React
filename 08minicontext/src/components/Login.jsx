f7f1c8bdeb17ceb783dd68fe25ce07d9e0799530
Sourabhkm7 <sourabhhithaldia@gmail.com>
2024-11-10 22:09:06 +0530

    Minin Project on context api

---
+++ 08minicontext/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import  {useState,useContext} from 'react'
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type='text' placeholder='username'/>
+      <input type='text' placeholder='password'/>
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default Login
